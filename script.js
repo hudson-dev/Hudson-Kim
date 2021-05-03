5c249c0f263ed5df3a8006759e6ded483d1e18cd
hudson-dev <hudson.n.kim@gmail.com>
2021-05-03 09:45:53 -0700

    Added jquery to change color of nav bar on scroll

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // @ts-nocheck
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".nav");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
+console.log("Start");
+
+$(document).ready(function(){       
+  var scroll_start = 0;
+  var startchange = $('#startchange');
+  var offset = startchange.offset();
+  if (startchange.length){
+  $(document).scroll(function() { 
+    console.log(offset);
+    scroll_start = $(this).scrollTop();
+    if(scroll_start > offset.top) {
+        $(".nav").css('color', 'orange');
+        $("<style>#logo::after{border-color: orange}</style>" ).appendTo("head");
+    } else {
+        $('.nav').css('color', 'black');
+        $("<style>#logo::after{border-color: blue}</style>" ).appendTo("head");
+    }});
+   }
 });
